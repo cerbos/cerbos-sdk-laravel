205ebee2cc47274aa1146dc1008f0a8b894deee9
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2023-09-05 15:53:50 +0300

    chore: Add LICENSE
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

---
+++ tests/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+// Copyright 2021-2023 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+declare(strict_types=1);
+
+namespace Cerbos\Sdk\Laravel\Test;
+
+class Test extends TestCase
+{
+    public function testDummy(): void {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+// Copyright 2021-2023 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+declare(strict_types=1);
+
+namespace Cerbos\Sdk\Laravel\Test;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
