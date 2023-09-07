8b34962d45086d869affcea8e2e71c807b376e08
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2023-09-07 14:08:34 +0300

    enhancement: Add configuration options (#2)
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- src/Provider/CerbosServiceProvider.php
+++ src/Provider/CerbosServiceProvider.php
@@ -7,16 +7,39 @@ namespace Cerbos\Sdk\Laravel\Provider;
 
 use Cerbos\Sdk\Builder\CerbosClientBuilder;
 use Cerbos\Sdk\CerbosClient;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class CerbosServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->publishes([
+                __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'cerbos.php' => $this->app->basePath() . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'cerbos.php',
+        ]);
+    }
+
     public function register(): void
     {
+        $this->mergeConfigFrom(__DIR__ . './../../config/cerbos.php', 'cerbos');
         $this->app->singleton(CerbosClient::class, function () {
-            return CerbosClientBuilder::newInstance(env('CERBOS_HOST') .":". env('CERBOS_PORT'))
-                ->withPlaintext(true)
-                ->build();
+            $caCert = File::get($this->app['config']['cerbos.caCertPath']);
+            $tlsCert = File::get($this->app['config']['cerbos.tlsCertPath']);
+            $tlsKey = File::get($this->app['config']['cerbos.tlsKeyPath']);
+
+            $cb = CerbosClientBuilder::newInstance($this->app['config']['cerbos.host'] .':'. $this->app['config']['cerbos.port'])
+                ->withPlaintext($this->app['config']['cerbos.plaintext']);
+            if ($caCert) {
+                $cb = $cb->withCaCertificate($caCert);
+            }
+            if ($tlsCert) {
+                $cb = $cb->withTlsCertificate($tlsCert);
+            }
+            if ($tlsKey) {
+                $cb = $cb->withTlsKey($tlsKey);
+            }
+
+            return $cb->build();
         });
     }
 
